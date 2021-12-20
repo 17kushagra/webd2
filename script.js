819ad0e858ecccea985e1b3597fbf18a8129c5aa
kushagra sharma <2001kushagrasharma@gmail.com>
2021-12-20 21:29:55 +0530

    first

---
+++ script.js
@@ -0,0 +1,15 @@
+// function myFunction() {
+//   var x = document.getElementById("myTopnav");
+//   if (x.className === "topnav") {
+//     x.className += " responsive";
+//   } else {
+//     x.className = "topnav";
+//   }
+// }
+ 
+let mainNav = document.getElementById("js-menu");
+let navBarToggle = document.getElementById("js-navbar-toggle");
+
+navBarToggle.addEventListener("click", function() {
+  mainNav.classList.toggle("active");
+});  
